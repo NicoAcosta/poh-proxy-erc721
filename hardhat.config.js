7f5de7afd6b6ed523dddcd31e3af6f579c415d6b
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2022-12-28 18:23:47 -0300

    add packages

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
+
+require('hardhat-contract-sizer')
+require('hardhat-abi-exporter')
+require('hardhat-gas-reporter')
+require('solidity-coverage')
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+	solidity: '0.8.17'
+}
